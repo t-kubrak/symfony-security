91a5c0bb81354364f3cefe8fe345cfd6e6b31512
tkubrak <taras.kubrak@dataart.com>
2021-05-08 21:46:31 +0300

    feat: add api endpoint and serializer

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -24,4 +24,16 @@ class AccountController extends BaseController
 
         ]);
     }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+
+        return $this->json($user, 200, [], [
+            'groups' => ['main'],
+        ]);
+    }
 }
