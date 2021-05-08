a6ffa4a9adb9bf299f850d96b804301c5c8fbb49
tkubrak <taras.kubrak@dataart.com>
2021-05-08 15:39:04 +0300

    feat: fetch the user; show profile

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
+
         return $this->render('account/index.html.twig', [
 
         ]);
